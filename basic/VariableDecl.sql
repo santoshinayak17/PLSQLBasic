3acb56ed873569827c046feb16cb6993dc695b80
santoshinayak17 <67648169+santoshinayak17@users.noreply.github.com>
2025-06-15 19:21:52 +0530

    variable commit

--- basic/VariableDecl.sql
+++ basic/VariableDecl.sql
@@ -6,3 +6,11 @@ ORDER BY table_name;
 select * from SYS.AUDIT_ACTIONS;
 DESC SYS.AUDIT_ACTIONS;
 
+SET SERVEROUTPUT ON;
+DECLARE
+    v_test VARCHAR2(15);
+BEGIN
+    v_test :='SANTOSHI';
+    DBMS_OUTPUT.PUT_LINE('I am ' || v_test);
+END;
+
